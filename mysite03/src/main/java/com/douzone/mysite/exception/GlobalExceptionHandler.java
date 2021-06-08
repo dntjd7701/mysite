dde85a2769014337587ba78fb24e17a867a82f05
dntjd7701 <dntjd7701@gmail.com>
2021-06-08 10:45:52 +0900

    logging #.assignment

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -3,23 +3,34 @@ package com.douzone.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 //@ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log LOGGER = LogFactory.getLog( GlobalExceptionHandler.class );
 	
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception e) {
 		// 1. logging
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		
-		
-		
-		System.out.println(errors);
+		// System.out.println(erros);
+		/**
+		 * 1. appender
+		 * 		file appender /log-mysite/exception.1.log.zip
+		 * 		10M (Archiving 정책)
+		 * 		1-10(rolling)
+		 * 		-console appender
+		 * 
+		 * 2. logger - com.douzone.mysite.exception, level(error), (console+file appender)
+		 *    logger - Root(default), level(debug), (console appender)
+		 * 
+		 */
+		LOGGER.error(errors.toString());
 		// 2. sorry~ page
-		
 		
 		// 3. 정상 종료 
 		

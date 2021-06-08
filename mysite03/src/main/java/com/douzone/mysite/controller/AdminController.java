e436beaa5b52b9e3d64870f3a4e0dc71dba240c8
dntjd7701 <dntjd7701@gmail.com>
2021-06-08 20:38:13 +0900

    #admin page-except profile

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -1,26 +1,35 @@
 package com.douzone.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.service.AdminService;
+import com.douzone.mysite.vo.SiteVo;
 
 
 @Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private AdminService adminService;
 
 	@RequestMapping("")
-	public String main() {
+	public String main(Model model) {
+		SiteVo vo = adminService.findMain();
+		model.addAttribute("siteVo", vo);
 		return "admin/main";
 	}
 	
 	
 	@RequestMapping(value="/main/update", method=RequestMethod.POST)
-	public String update() {
-		return "redirce:/admin";
+	public String update(SiteVo vo) {
+		adminService.updateMain(vo);
+		return "redirect:/admin";
 	}
 	
 

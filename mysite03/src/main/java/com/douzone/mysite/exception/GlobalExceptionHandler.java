3d2be56d171f1b9fd5c93cdf79f6c9315996c567
dntjd7701 <dntjd7701@gmail.com>
2021-06-10 15:25:13 +0900

    Gallery #. set sources.

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.douzone.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -10,6 +11,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.douzone.mysite.dto.JsonResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 //@ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log LOGGER = LogFactory.getLog( GlobalExceptionHandler.class );
@@ -19,17 +23,26 @@ public class GlobalExceptionHandler {
 			HttpServletRequest request,
 			HttpServletResponse response,
 			Exception e) throws Exception {
+		
 		// 1. logging
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		LOGGER.error(errors.toString());
-		
 		// 2. 요청 구분
 		// 만약, JSON 요청인 경우라면 request header의 Accept에 application/json
 		// 만약, HTML 요청인 경우라면 request header의 Accept에 text/html
 		String accept = request.getHeader("accept");
 		if(accept.matches(	".*application/json.*")) {
 			// 3. Json 응답. 
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			
+			JsonResult result = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(result);
+			
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("UTF-8"));
+			os.close();
 			
 		} else {
 			// 3. 사과 페이지 가기 (정상 종료)
